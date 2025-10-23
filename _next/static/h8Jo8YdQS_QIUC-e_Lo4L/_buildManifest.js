49d3e8ac3e3d7908c315536d23a71e29c0fc2d9f
garrettmoss <garrettmoss1@gmail.com>
2025-10-22 21:30:13 -0700

    Updates

---
+++ _next/static/h8Jo8YdQS_QIUC-e_Lo4L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-454e5a2408b7ff2e.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
